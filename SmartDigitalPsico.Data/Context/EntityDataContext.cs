3f24639eec86b9d23efab2ea40ff1c1b3d9e2ce3
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-03-30 19:21:30 -0300

    Refatoracao do repositorio e servico reduzindo complexidade e duplicidade - configuracao de entidades

--- SmartDigitalPsico.Data/Context/EntityDataContext.cs
+++ SmartDigitalPsico.Data/Context/EntityDataContext.cs
@@ -7,53 +7,34 @@ namespace SmartDigitalPsico.Data.Context
     {
         protected EntityDataContext(DbContextOptions<SmartDigitalPsicoDataContext> options) : base(options)
         {
+        }
 
-        } 
-        #region Statics 
+        #region DBsets
         public virtual DbSet<Gender> Genders { get; set; }
         public virtual DbSet<Office> Offices { get; set; }
         public virtual DbSet<RoleGroup> RoleGroups { get; set; }
         public virtual DbSet<Specialty> Specialties { get; set; }
         public virtual DbSet<ApplicationLanguage> ApplicationLanguages { get; set; }
         public virtual DbSet<ApplicationConfigSetting> ApplicationConfigSettings { get; set; }
-
         public virtual DbSet<ApplicationCacheLog> ApplicationCacheLogs { get; set; }
-
         public virtual DbSet<InfoTag> InfoTags { get; set; }
-
-        #endregion
-
-        #region Principais
-
         public virtual DbSet<User> Users { get; set; }
-
         public virtual DbSet<Medical> Medicals { get; set; }
-
         public virtual DbSet<Patient> Patients { get; set; }
-
         public virtual DbSet<PatientAdditionalInformation> PatientAdditionalInformations { get; set; }
-
         public virtual DbSet<PatientHospitalizationInformation> PatientHospitalizationInformations { get; set; }
-
         public virtual DbSet<PatientMedicationInformation> PatientMedicationInformations { get; set; }
         public virtual DbSet<PatientRecord> PatientRecords { get; set; }
-
         public virtual DbSet<PatientNotificationMessage> PatientNotificationMessages { get; set; }
-
         public virtual DbSet<PatientFile> PatientFiles { get; set; }
-
         public virtual DbSet<MedicalFile> MedicalFiles { get; set; }
-
         public virtual DbSet<MedicalCalendar> MedicalCalendars { get; set; }
-
-        #endregion Principais
+        #endregion DBsets
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            optionsBuilder.EnableSensitiveDataLogging();
-
-
+            //optionsBuilder.EnableSensitiveDataLogging();
             base.OnConfiguring(optionsBuilder);
         }
     }
