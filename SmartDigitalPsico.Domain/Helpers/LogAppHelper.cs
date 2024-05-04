3a883a24a94e75d4c69df00b79beefdf12a19fe0
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-05-04 20:13:28 -0300

    Docker ajuste - Finalmente 2.1

--- SmartDigitalPsico.Domain/Helpers/LogAppHelper.cs
+++ SmartDigitalPsico.Domain/Helpers/LogAppHelper.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Serilog;
 using Serilog.Events;
+using SmartDigitalPsico.Domain.VO;
 using System.Diagnostics;
 using System.Reflection;
+using System.Text;
 
 namespace SmartDigitalPsico.Domain.Helpers
 {
     public static class LogAppHelper
-    { 
+    {
         public static string GetDurationStopwatch(Stopwatch stopwatch)
         {
             return TimeSpan.FromMilliseconds(stopwatch.ElapsedMilliseconds).ToString(@"hh\:mm\:ss");
@@ -29,27 +31,55 @@ namespace SmartDigitalPsico.Domain.Helpers
         public static Serilog.Core.Logger CreateLogger(IConfiguration configuration)
         {
             return new LoggerConfiguration()
-                      .ReadFrom.Configuration(configuration) 
+                      .ReadFrom.Configuration(configuration)
                       .Enrich.FromLogContext()
                       .CreateLogger();
         }
 
-        public static void PrintLogInformationVersionProduct(ILogger logger)
-        {
-            logger.Information("PRODUCT INFORMATION");
-            logger.Information("*******");
 
+        public static AppInformationVersionProduct? GetInformationVersionProduct()
+        {
             var assembly = Assembly.GetEntryAssembly();
             if (assembly != null)
             {
-                logger.Information("Name: {Name}", assembly.GetName().Name);
-                logger.Information("Version: {Version}", assembly.GetName().Version);
+                var assemblyApp = assembly.GetName();
+
+                return new AppInformationVersionProduct() { Name = assemblyApp.Name, Version = assemblyApp.Version?.ToString() };
+            }
+            return null;
+        }
+
+        public static string ShowInformationVersionProduct()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("******* PRODUCT INFORMATION ******* {0}", Environment.NewLine);
+            var assemblyApp = GetInformationVersionProduct();
+            if (assemblyApp != null)
+            {
+                sb.AppendFormat("Name: {0} - Version: {1} {2}", assemblyApp.Name, assemblyApp.Version, Environment.NewLine);
+            }
+            else
+            {
+                sb.AppendFormat("Assembly information could not be retrieved.{0}", Environment.NewLine);
+            }
+            sb.AppendFormat("******* PRODUCT INFORMATION ******* {0}", Environment.NewLine);
+
+            return sb.ToString();
+        }
+
+        public static void PrintLogInformationVersionProduct(Serilog.ILogger logger)
+        {
+            logger.Information("******* PRODUCT INFORMATION *******");
+            var assemblyApp = GetInformationVersionProduct();
+            if (assemblyApp != null)
+            {
+                logger.Information("Name: {Name} - Version: {Version}", assemblyApp.Name, assemblyApp.Version);
             }
             else
             {
                 logger.Information("Assembly information could not be retrieved.");
             }
-            logger.Information("*******");
+            logger.Information("******* PRODUCT INFORMATION *******");
         }
     }
 }
