135526af561cc02e2471ceb1290262f2d4fdd7ec
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-05-18 19:52:19 -0300

    Criando testes unitarios - Repository

---
+++ SmartDigitalPsico.Data.Test/Configure/BaseTests.cs
@@ -0,0 +1,26 @@
+﻿using SmartDigitalPsico.Data.Tests.Context;
+
+namespace SmartDigitalPsico.Data.Test.Configure
+{
+    [TestFixture]
+    public class BaseTests
+    {
+        protected SmartDigitalPsicoDataContextTest? _mockContext;
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            // Arrange
+            _mockContext = new SmartDigitalPsicoDataContextTest();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (_mockContext != null)
+            {
+                _mockContext.Dispose();
+            }
+        } 
+    }
+}
---
+++ SmartDigitalPsico.Data.Test/Repository/GenderRepositoryTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartDigitalPsico.Data.Repository.SystemDomains;
+using SmartDigitalPsico.Data.Test.Configure;
+using SmartDigitalPsico.Data.Test.DataMock;
+using SmartDigitalPsico.Data.Tests.Context;
+using SmartDigitalPsico.Domain.ModelEntity;
+
+namespace SmartDigitalPsico.Data.Tests.Repository
+{
+    [TestFixture]
+    public class GenderRepositoryTests : BaseTests
+    { 
+        private GenderRepository? _entityRepository;
+
+        [SetUp]
+        public override void Setup()
+        {
+            var mockData = GenderMockHelper.GetMock().AsQueryable();
+            // Arrange 
+            SetupContext(mockData);
+        }
+        private void SetupContext(IQueryable<Gender> mockData)
+        {
+            var mockDataList = mockData.ToList();
+            // Arrange
+            _mockContext = new SmartDigitalPsicoDataContextTest();
+             
+            _mockContext.Genders.AddRange(mockDataList);
+            _mockContext.SaveChanges();             
+        } 
+
+        [Test]
+        public async Task Create_Success()
+        {
+            // Arrange
+            var mockFull = GenderMockHelper.GetMock().AsQueryable();
+            SetupContext(mockFull);
+            var mockData = GenderMockHelper.GetMock().Take(1).AsQueryable().First();
+            var data = createEntity(mockData);
+            _mockContext = _mockContext ?? new SmartDigitalPsicoDataContextTest();
+
+            // Inicialize  Repository
+            _entityRepository = new GenderRepository(_mockContext);
+
+            // Act
+            var result = await _entityRepository.Create(data);
+
+            var target = await _mockContext.Genders.FirstAsync(e => e.Id == data.Id);
+
+            //Assert  
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.EqualTo(data));
+                Assert.That(target, Is.Not.Null);
+            });
+        } 
+        private Gender createEntity(Gender mockData)
+        {
+            return new Gender()
+            {
+                Id = (long)int.MaxValue,
+                ModifyDate = mockData.ModifyDate,
+                CreatedDate = mockData.CreatedDate,
+                Description = mockData.Description,
+                Enable = mockData.Enable,
+                Language = mockData.Language,
+                LastAccessDate = mockData.LastAccessDate
+            };
+        }
+
+    }
+}
