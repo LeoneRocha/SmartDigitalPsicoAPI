656c9861c162002965b47342e39f2b54f9743971
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-05-24 11:03:02 -0300

    Separando ambientes

--- SmartDigitalPsico.WebAPI/Controllers/v1/SystemDomains/AppInformationVersionProductController.cs
+++ SmartDigitalPsico.WebAPI/Controllers/v1/SystemDomains/AppInformationVersionProductController.cs
@@ -1,20 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using SmartDigitalPsico.Domain.Helpers;
+using SmartDigitalPsico.Domain.VO;
+using System.Collections.Generic;
 
 namespace SmartDigitalPsico.WebAPI.Controllers.v1.SystemDomains
 {
-    [ApiController]  
+    [ApiController]
     [Route("api/[controller]/v1")]
     public class AppInformationVersionProductController : ControllerBase
     {
         public AppInformationVersionProductController()
         {
-        } 
-        [HttpGet("GetAppInformationVersionProduct")]
-        public async Task<ActionResult<string>> Get()
+        }
+        [HttpGet("GetAppInformationVersionProductString")]
+        public async Task<ActionResult<string>> GetString()
         {
             await Task.FromResult(0);
-            return Ok(LogAppHelper.ShowInformationVersionProduct());
+            return Ok(LogAppHelper.ShowInformationVersionProductString());
         }
+
+        [HttpGet("GetAppInformationVersionProduct")]
+        public async Task<ActionResult<List<AppInformationVersionProductVO>>> Get()
+        {
+            await Task.FromResult(0);
+            var responseVO = LogAppHelper.GetInformationVersionProduct();
+            List<AppInformationVersionProductVO> response = new List<AppInformationVersionProductVO> { responseVO };
+            if (response.Count <= 0)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+        } 
     }
 }
