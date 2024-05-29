3ed339ee643974fa51b4e4f8826861ceef5c2612
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-05-29 16:49:11 -0300

    Ajuste de consultas strings

--- SmartDigitalPsico.Data/Repository/Principals/MedicalRepository.cs
+++ SmartDigitalPsico.Data/Repository/Principals/MedicalRepository.cs
@@ -3,6 +3,7 @@ using SmartDigitalPsico.Data.Context;
 using SmartDigitalPsico.Domain.Interfaces.Repository;
 using SmartDigitalPsico.Domain.ModelEntity;
 using SmartDigitalPsico.Data.Repository.Generic;
+using MySql.EntityFrameworkCore.Extensions;
 
 namespace SmartDigitalPsico.Data.Repository.Principals
 {
@@ -18,7 +19,7 @@ namespace SmartDigitalPsico.Data.Repository.Principals
         }
         public async override Task<Medical> FindByID(long id)
         {
-            return await _dataset 
+            return await _dataset
                 .Include(e => e.User)
                 .Include(e => e.Office)
                 .Include(e => e.MedicalSpecialties)
@@ -40,24 +41,30 @@ namespace SmartDigitalPsico.Data.Repository.Principals
 
         public async Task<Medical?> FindByEmail(string email)
         {
-            Medical? entityResult = await _dataset
+            var normalizedEmail = email.ToUpper();
+
+            Medical? entityResult = (await _dataset
                 .AsNoTracking()
-                .FirstOrDefaultAsync(p => p.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
+                .Where(p => p.Email == normalizedEmail).ToListAsync()).ToList()
+                .FirstOrDefault(p => p.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
 
             return entityResult;
-        } 
+        }
         public async Task<Medical?> FindByAccreditation(string accreditation)
         {
-            Medical? entityResult = await _dataset
+            var normalizedAccreditation = accreditation.ToUpper();
+
+            Medical? entityResult = (await _dataset
                 .AsNoTracking()
                 .Include(e => e.User)
                 .Include(e => e.Office)
                 .Include(e => e.MedicalSpecialties)
                 .ThenInclude(ms => ms.Specialty)
                 .Include(e => e.CreatedUser)
-                .FirstOrDefaultAsync(p => p.Accreditation.Equals(accreditation, StringComparison.OrdinalIgnoreCase));
+                .Where(p => p.Accreditation == normalizedAccreditation).ToListAsync())
+                .FirstOrDefault(p => p.Accreditation.Equals(normalizedAccreditation, StringComparison.OrdinalIgnoreCase));
 
             return entityResult;
-        }  
+        }
     }
 }
