b2bb96f94a82245c9faaebcbf9c3851be75faeca
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-09-30 22:16:05 -0300

    Ajsute sonar cloud

---
+++ SmartDigitalPsico.Domain/DTO/Domains/AuditDataSelectiveEntityLogBaseDto.cs
@@ -0,0 +1,21 @@
+﻿using SmartDigitalPsico.Domain.DTO.User;
+
+namespace SmartDigitalPsico.Domain.DTO.Domains
+{
+    public abstract class AuditDataSelectiveEntityLogBaseDto
+    {
+        public string RowKey { get; set; } = string.Empty;
+        public string PartitionKey { get; set; } = string.Empty;
+
+        public string TableName { get; set; } = string.Empty;
+        public string Operation { get; set; } = string.Empty;
+        public string KeyValue { get; set; } = string.Empty;
+        public string OldValues { get; set; } = string.Empty;
+        public string NewValues { get; set; } = string.Empty;
+        public DateTime AuditDate { get; set; } = DateTime.UtcNow;
+        public string? UserAuditedLogin { get; set; } = null;
+
+        public GetUserDto? UserAudited { get; set; }
+        public long? UserAuditedId { get; set; }
+    }
+} 
--- SmartDigitalPsico.Domain/DTO/Domains/UpdateDTOs/UpdateAuditDataSelectiveEntityLogDto.cs
+++ SmartDigitalPsico.Domain/DTO/Domains/UpdateDTOs/UpdateAuditDataSelectiveEntityLogDto.cs
@@ -1,22 +1,9 @@
-using SmartDigitalPsico.Domain.DTO.Contracts;
-using SmartDigitalPsico.Domain.DTO.User;
+using SmartDigitalPsico.Domain.Interfaces;
 
 namespace SmartDigitalPsico.Domain.DTO.Domains.UpdateDTOs
 {
-    public class UpdateAuditDataSelectiveEntityLogDto : EntityDtoBaseDomain
+    public class UpdateAuditDataSelectiveEntityLogDto : AuditDataSelectiveEntityLogBaseDto, IEntityDto
     {
-        public string RowKey { get; set; } = string.Empty;
-        public string PartitionKey { get; set; } = string.Empty;
-
-        public string TableName { get; set; } = string.Empty;
-        public string Operation { get; set; } = string.Empty;
-        public string KeyValue { get; set; } = string.Empty;
-        public string OldValues { get; set; } = string.Empty;
-        public string NewValues { get; set; } = string.Empty;
-        public DateTime AuditDate { get; set; } = DateTime.UtcNow;
-        public string? UserAuditedLogin { get; set; } = null;
-
-        public GetUserDto? UserAudited { get; set; }
-      
+        public long Id { get; set; }
     }
 }
