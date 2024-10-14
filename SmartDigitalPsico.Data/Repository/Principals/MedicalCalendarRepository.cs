fc5b698a2adb172495c0b2fa0fa034534382f06f
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-10-14 00:04:28 -0300

    Ajuste sonar

--- SmartDigitalPsico.Data/Repository/Principals/MedicalCalendarRepository.cs
+++ SmartDigitalPsico.Data/Repository/Principals/MedicalCalendarRepository.cs
@@ -46,9 +46,9 @@ namespace SmartDigitalPsico.Data.Repository.Principals
         public async Task<MedicalCalendar[]> GetMedicalCalendarsForMedicalAsync(long medicalId, DateTime startDate, DateTime endDate)
         {
             return await _context.MedicalCalendars
-                .Where(mc => mc.Enable == true && mc.MedicalId == medicalId && mc.StartDateTime >= startDate && mc.EndDateTime <= endDate)                
+                .Where(mc => mc.Enable && mc.MedicalId == medicalId && mc.StartDateTime >= startDate && mc.EndDateTime <= endDate)
                 .Include(x => x.Patient)
                 .ToArrayAsync();
         }
     }
-} 
+}
