6a0b9f0d3cbbb16a8d2776e1d5678ce1a6a9e997
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2025-02-12 21:52:18 -0300

    Ajuste de menssagens

--- SmartDigitalPsico.Domain/Constants/I18nKeyConstants/MedicalCalendarKeyConstants.cs
+++ SmartDigitalPsico.Domain/Constants/I18nKeyConstants/MedicalCalendarKeyConstants.cs
@@ -7,6 +7,7 @@
         public const string CalendarUpdated = "Calendar_Updated";
         public const string CalendarSuccess = "Calendar_Success";
         public const string SchedulesDeletedSuccessfully = "Schedules_Deleted_Successfully";
-         
+        public const string Schedule_Appointment_Success = "Schedule_Appointment_Success";
+        public const string Cancel_Appointment_Success = "Cancel_Appointment_Success";
     }
 }
---
+++ SmartDigitalPsico.Domain/Constants/I18nKeyConstants/UserKeyConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartDigitalPsico.Domain.Constants.I18nKeyConstants
+{
+
+    public static class UserKeyConstants
+    { 
+        public const string User_Not_Found = "User_Not_Found"; 
+         
+    }
+}
