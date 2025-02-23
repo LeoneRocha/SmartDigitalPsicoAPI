504f1f26544c3b7c6a9c17686151ab1159d56200
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2025-02-23 01:19:39 -0300

    Ajuste parte 1 email template

--- SmartDigitalPsico.Domain/DTO/Domains/EmailTemplateBaseDto.cs
+++ SmartDigitalPsico.Domain/DTO/Domains/EmailTemplateBaseDto.cs
@@ -1,11 +1,10 @@
-﻿namespace SmartDigitalPsico.Domain.DTO.Domains
+﻿using SmartDigitalPsico.Domain.DTO.Contracts;
+
+namespace SmartDigitalPsico.Domain.DTO.Domains
 {
-    public abstract class EmailTemplateBaseDto
+    public abstract class EmailTemplateBaseDto : EntityDtoBaseDomain
     {
         public string Subject { get; set; } = string.Empty;
-        public string Body { get; set; } = string.Empty;
-
-        public string Description { get; set; } = string.Empty;
-        public string Language { get; set; } = "en";
+        public string Body { get; set; } = string.Empty; 
     }
 }
