804727cb85e101fd0c4b8498dd273c8b120ef626
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2025-02-25 21:03:24 -0300

    Automaticando inclusao dos configurations das entidades

---
+++ SmartDigitalPsico.Data/Context/Configure/Helper/ModelBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartDigitalPsico.Domain.Enuns;
+using System.Reflection;
+
+namespace SmartDigitalPsico.Data.Context.Configure.Helper
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void AddConfigurationEntities(this ModelBuilder modelBuilder, ETypeDataBase eDataBaseType, Assembly assembly, List<Type> manuallyConfiguredTypes)
+        {
+            Type[] configTypes = ListClassConfiguration(assembly, manuallyConfiguredTypes);
+
+            foreach (var configType in configTypes)
+            {
+                dynamic configInstance = Activator.CreateInstance(configType, eDataBaseType)!;
+                modelBuilder.ApplyConfiguration(configInstance);
+            }
+        }
+
+        private static Type[] ListClassConfiguration(Assembly assembly, List<Type> manuallyConfiguredTypes)
+        {
+            return assembly.GetTypes().Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>)) && !manuallyConfiguredTypes.Contains(t) && t.Name.EndsWith("Configuration")).ToArray();
+        }
+    }
+}
