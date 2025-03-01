0b3c7ab1c857e3177b6d753b76ad73939fa09690
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2025-03-01 15:27:37 -0300

    Ajute notificacoe

---
+++ SmartDigitalPsico.Data/Context/Configure/Mock/NotificationRulesMockData.cs
@@ -0,0 +1,106 @@
+﻿using SmartDigitalPsico.Domain.Enuns;
+using SmartDigitalPsico.Domain.Helpers;
+using SmartDigitalPsico.Domain.ModelEntity;
+
+namespace SmartDigitalPsico.Data.Context.Configure.Mock
+{
+    public static class NotificationRulesMockData
+    {
+        public static NotificationRules[] GetMock()
+        {
+            return new[]
+            {
+            // Envio 24 horas antes do agendamento
+            new NotificationRules
+            {
+                Id = 1,
+                Enable = true,
+                MedicalId = 1,
+                IsEnabled = true,
+                IntervalType = EIntervalNotificationType.Hours,
+                IntervalValue = 24,
+                IsBefore = true,
+                ENotificationServiceType = new [] { ENotificationServiceType.Email },
+                Description = "Envio 24 horas antes do agendamento",
+                Language = "pt-BR",
+                NotificationType = ENotificationType.BeforeAppointment,
+                CreatedDate = DateHelper.GetDateTimeNowFromUtc(),
+                ModifyDate = DateHelper.GetDateTimeNowFromUtc(),
+                LastAccessDate = DateHelper.GetDateTimeNowFromUtc()
+            },
+            // Envio 3 dias antes do agendamento
+            new NotificationRules
+            {
+                Id = 2,
+                Enable = true,
+                MedicalId = 1,
+                IsEnabled = true,
+                IntervalType = EIntervalNotificationType.Days,
+                IntervalValue = 3,
+                IsBefore = true,
+                ENotificationServiceType = new [] { ENotificationServiceType.Email },
+                Description = "Envio 3 dias antes do agendamento",
+                Language = "pt-BR",
+                NotificationType = ENotificationType.BeforeAppointment,
+                CreatedDate = DateHelper.GetDateTimeNowFromUtc(),
+                ModifyDate = DateHelper.GetDateTimeNowFromUtc(),
+                LastAccessDate = DateHelper.GetDateTimeNowFromUtc()
+            },
+            // Envio 1 hora antes do agendamento
+            new NotificationRules
+            {
+                Id = 3,
+                Enable = true,
+                MedicalId = 1,
+                IsEnabled = true,
+                IntervalType = EIntervalNotificationType.Hours,
+                IntervalValue = 1,
+                IsBefore = true,
+                ENotificationServiceType = new [] { ENotificationServiceType.Email },
+                Description = "Envio 1 hora antes do agendamento",
+                Language = "pt-BR",
+                NotificationType = ENotificationType.BeforeAppointment,
+                CreatedDate = DateHelper.GetDateTimeNowFromUtc(),
+                ModifyDate = DateHelper.GetDateTimeNowFromUtc(),
+                LastAccessDate = DateHelper.GetDateTimeNowFromUtc()
+            },
+            // Envio 15 minutos antes do agendamento
+            new NotificationRules
+            {
+                Id = 4,
+                Enable = true,
+                MedicalId = 1,
+                IsEnabled = true,
+                IntervalType = EIntervalNotificationType.Minutes,
+                IntervalValue = 15,
+                IsBefore = true,
+                ENotificationServiceType = new [] { ENotificationServiceType.Email },
+                Description = "Envio 15 minutos antes do agendamento",
+                Language = "pt-BR",
+                NotificationType = ENotificationType.BeforeAppointment,
+                CreatedDate = DateHelper.GetDateTimeNowFromUtc(),
+                ModifyDate = DateHelper.GetDateTimeNowFromUtc(),
+                LastAccessDate = DateHelper.GetDateTimeNowFromUtc()
+            }, 
+            // Lembrete de pagamento (3 dias antes do vencimento)
+            new NotificationRules
+            {
+                Id = 5,
+                Enable = true,
+                MedicalId = 1,
+                IsEnabled = true,
+                IntervalType = EIntervalNotificationType.Days,
+                IntervalValue = 3,
+                IsBefore = true,
+                ENotificationServiceType = new [] { ENotificationServiceType.Email },
+                Description = "Lembrete de pagamento (3 dias antes do vencimento)",
+                Language = "pt-BR",
+                NotificationType = ENotificationType.PaymentReminder,
+                CreatedDate = DateHelper.GetDateTimeNowFromUtc(),
+                ModifyDate = DateHelper.GetDateTimeNowFromUtc(),
+                LastAccessDate = DateHelper.GetDateTimeNowFromUtc()
+            }
+        };
+        }
+    }
+}
--- SmartDigitalPsico.Domain/Interfaces/Repository/INotificationRulesRepository.cs
+++ SmartDigitalPsico.Domain/Interfaces/Repository/INotificationRulesRepository.cs
@@ -1,8 +1,10 @@
-﻿using SmartDigitalPsico.Domain.ModelEntity;
+﻿using SmartDigitalPsico.Domain.Enuns;
+using SmartDigitalPsico.Domain.ModelEntity;
 
 namespace SmartDigitalPsico.Domain.Interfaces.Repository
 {
     public interface INotificationRulesRepository : IEntityBaseRepository<NotificationRules>
-    { 
+    {
+        Task<List<NotificationRules>> GetNotificationRulesAsync(ENotificationType notificationType, bool isEnabled, long medicalId);
     }
 } 
